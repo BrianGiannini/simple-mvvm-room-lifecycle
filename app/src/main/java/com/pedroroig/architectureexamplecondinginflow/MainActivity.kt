78dd4a875a24b92613aa9ea4e085c107773231bf
Pedro Roig <pedrorg18@hotmail.com>
2019-12-31 16:49:56 +0100

    create view model. Add it to activity and get all notes

--- app/src/main/java/com/pedroroig/architectureexamplecondinginflow/MainActivity.kt
+++ app/src/main/java/com/pedroroig/architectureexamplecondinginflow/MainActivity.kt
@@ -2,11 +2,25 @@ package com.pedroroig.architectureexamplecondinginflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var vm: NoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        vm = ViewModelProviders.of(this)[NoteViewModel::class.java]
+
+        vm.getAllNotes().observe(this, Observer {
+            Toast.makeText(this, "Notes: $it", LENGTH_LONG).show()
+            Log.i("Notes observed", "$it")
+        })
     }
 }
