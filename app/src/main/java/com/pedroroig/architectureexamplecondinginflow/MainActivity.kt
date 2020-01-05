26a2e3b84b52c1116eb68f12e10eab7a14e71110
Pedro Roig <pedrorg18@hotmail.com>
2020-01-05 18:01:22 +0100

    implement all notes deletion by menu button

--- app/src/main/java/com/pedroroig/architectureexamplecondinginflow/MainActivity.kt
+++ app/src/main/java/com/pedroroig/architectureexamplecondinginflow/MainActivity.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -85,6 +87,22 @@ class MainActivity : AppCompatActivity() {
         } else {
             Toast.makeText(this, "Note not saved!", Toast.LENGTH_SHORT).show()
         }
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.delete_all_notes -> {
+                vm.deleteAllNotes()
+                Toast.makeText(this, "All notes deleted!", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
